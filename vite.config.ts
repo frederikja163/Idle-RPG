bb119abf9bee5d95cbc35a1fd34af9daebabc6cb
OliverEC <oliverec04@gmail.com>
2025-05-07 22:31:25 +0200

    setup vite

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+﻿import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    root:
+      'src/front-end',
+    resolve:
+      {
+        alias: {
+          '@':
+            path.resolve(__dirname, 'src/'),
+        }
+        ,
+      }
+    ,
+    server: {
+      port: Number(env.VITE_FRONTEND_PORT ?? 3000),
+    }
+    ,
+    build: {
+      outDir: '../../dist',
+      emptyOutDir:
+        true,
+    }
+    ,
+  }
+});
