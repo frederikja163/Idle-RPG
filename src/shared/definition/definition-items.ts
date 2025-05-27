3866bf7f0d442074d4f2685a03de5864f50c4f6c
Frederikja <fjaplv64@gmail.com>
2025-05-27 23:49:09 +0200

    Change item definitions

--- src/shared/definition/definition-items.ts
+++ src/shared/definition/definition-items.ts
@@ -8,40 +8,53 @@ ore('Calcite');
 ore('Flourite');
 ore('Apatite');
 
-wood('Balsa');
-wood('Pine');
-wood('Cedar');
-wood('Cherry');
-wood('Oak');
+refined('Talc');
+refined('Gypsum');
+refined('Calcite');
+refined('Flourite');
+refined('Apatite');
+
+log('Balsa');
+log('Pine');
+log('Cedar');
+log('Cherry');
+log('Oak');
+
+plank('Balsa');
+plank('Pine');
+plank('Cedar');
+plank('Cherry');
+plank('Oak');
+console.log(items);
 
 function ore(name: string) {
-  const id = name.toLowerCase();
-  item(`ore_${id}`, `${name} ore`, ItemTag.Resource, ItemTag.Tool);
-  item(`refined_${id}`, `Refined ${id}`, ItemTag.Resource, ItemTag.Tool);
+  const id = `ore_${name.toLowerCase()}`;
+  items.set(id, itemDef(id, `${name} ore`, ItemTag.Resource));
 }
-
-function wood(name: string) {
-  const id = name.toLowerCase();
-  item(`log_${id}`, `${name} log`, ItemTag.Resource);
-  item(`plank_${id}`, `${name} plank`, ItemTag.Resource);
+function refined(name: string) {
+  const id = `refined_${name.toLowerCase()}`;
+  items.set(id, itemDef(id, `Refined ${name}`, ItemTag.Resource));
 }
-
-function item(id: string, display: string, ...tags: ItemTag[]) {
-  items.set(id, { id, display, tags });
+function log(name: string) {
+  const id = `log_${name.toLowerCase()}`;
+  items.set(id, itemDef(id, `${name} log`, ItemTag.Resource));
+}
+function plank(name: string) {
+  const id = `plank_${name.toLowerCase()}`;
+  items.set(id, itemDef(id, `${name} plank`, ItemTag.Resource));
 }
 
 export enum ItemTag {
   Resource,
-  Tool,
 }
 
-export const itemTagDisplayMap = new Map<ItemTag, string>([
-  [ItemTag.Resource, 'Resource'],
-  [ItemTag.Tool, 'Tool'],
-]);
+export const itemTagDisplayMap = new Map<ItemTag, string>([[ItemTag.Resource, 'Resource']]);
 
 export type ItemDef = {
   id: ItemId;
   display: string;
   tags: ItemTag[];
 };
+function itemDef(id: string, display: string, ...tags: ItemTag[]): ItemDef {
+  return { id, display, tags };
+}
