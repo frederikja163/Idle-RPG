e5e6c4c5e4b587d45619618c590f55ef7e87cbe6
Frederikja <fjaplv64@gmail.com>
2025-05-31 15:12:52 +0200

    Refactor definitions

--- src/back-end/container.ts
+++ src/back-end/container.ts
@@ -1,10 +1,11 @@
-import "./core/db/db";
-import "./core/server/sockets/socket-hub";
-import "./core/events/orchestrators/socket-lifecycle-orchestrator";
+import '@/shared/definition/definitions';
+import './core/db/db';
+import './core/server/sockets/socket-hub';
+import './core/events/orchestrators/socket-lifecycle-orchestrator';
 
-import "./features/auth/auth-google-controller";
-import "./features/auth/auth-logout-controller";
-import "./features/profile/profile-controller";
-import "./features/skill/skill-controller";
-import "./features/activity/activity-controller";
-import "./features/item/item-controller";
+import './features/auth/auth-google-controller';
+import './features/auth/auth-logout-controller';
+import './features/profile/profile-controller';
+import './features/skill/skill-controller';
+import './features/activity/activity-controller';
+import './features/item/item-controller';
--- src/shared/util/util-skills.ts
+++ src/shared/util/util-skills.ts
@@ -3,7 +3,7 @@ import type { Skill } from '../definition/schema/types/types-skills';
 const maxLevel = 50;
 
 export function xpToReachNext(nextLevel: number) {
-  return Math.floor(5 * Math.pow(Math.pow(10, Math.log10(2) / 5), nextLevel));
+  return Math.floor(25 * Math.pow(2, nextLevel / 5));
 }
 
 export const xpAccum: number[] = [0, xpToReachNext(1)];
@@ -13,7 +13,7 @@ for (let index = 2; index <= maxLevel; index++) {
 
 export function addXp(skill: Skill, xp: number) {
   skill.xp += xp;
-  while (skill.xp > xpAccum[skill.level]) {
+  while (skill.xp >= xpAccum[skill.level + 1]) {
     skill.level += 1;
   }
   if (skill.level >= maxLevel) {
