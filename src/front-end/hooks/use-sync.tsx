312da6d8622e9465d9abcbaba791cee2cd060430
OliverEC <oliverec04@gmail.com>
2025-07-08 21:43:14 +0200

    fixed imports

--- src/front-end/hooks/use-sync.tsx
+++ src/front-end/hooks/use-sync.tsx
@@ -1,10 +1,10 @@
 import { useCallback } from 'react';
 import { routes } from '@/front-end/router/routes.ts';
 import { useSetAtom } from 'jotai/index';
-import { resetAtomsAtom, selectedProfileIdAtom } from '@/front-end/state/atoms.tsx';
-import { useSocket } from '@/front-end/state/socket-provider.tsx';
 import { useNavigate } from 'react-router-dom';
 import { useAtom } from 'jotai';
+import { useSocket } from '@/front-end/providers/socket-provider.tsx';
+import { resetAtomsAtom, selectedProfileIdAtom } from '@/front-end/store/atoms.tsx';
 
 export const useSync = () => {
   const navigate = useNavigate();
