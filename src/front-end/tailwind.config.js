5da09898e9611a98dbabf8727dc146587e9a3eff
OliverEC <oliverec04@gmail.com>
2025-07-25 21:00:32 +0200

    some

--- src/front-end/tailwind.config.js
+++ src/front-end/tailwind.config.js
@@ -1,10 +1,7 @@
 ﻿module.exports = {
-    content: [
-        "./src/front-end/**/*.{html,js,ts,jsx,tsx}",
-        "./src/front-end/index.html"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: ['./src/front-end/**/*.{html,js,ts,jsx,tsx}', './src/front-end/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
