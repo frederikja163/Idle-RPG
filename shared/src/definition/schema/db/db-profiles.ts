cb8d746b07dc38947b83f4d6f87edb022b7f5b54
Frederikja <fjaplv64@gmail.com>
2025-09-23 17:20:26 +0200

    Too many small changes

--- shared/src/definition/schema/db/db-profiles.ts
+++ shared/src/definition/schema/db/db-profiles.ts
@@ -3,22 +3,18 @@ import { integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core
 import { userProfilesTable } from './db-userprofiles';
 import { itemsTable } from './db-items';
 import { skillsTable } from './db-skills';
-import { timestampNow } from './db-types';
+import { timestampNowSql } from './db-types';
 
 export const profilesTable = sqliteTable(
   'profiles',
   {
     id: text('id').primaryKey().$defaultFn(crypto.randomUUID.bind(crypto)),
     name: text('name').notNull(),
-    firstLogin: integer('first_login')
-      .notNull()
-      .default(sql`${timestampNow}`),
-    lastLogin: integer('last_login')
-      .notNull()
-      .default(sql`${timestampNow}`),
+    firstLogin: integer('first_login').notNull().default(timestampNowSql),
+    lastLogin: integer('last_login').notNull().default(timestampNowSql),
     activity: text('activity', { mode: 'json' })
       .notNull()
-      .default(sql`{"type": "none", "start": ${timestampNow}}`),
+      .default(sql`'{"type": "none", "start": 0}'`),
     settings: text('settings').notNull().default(''),
   },
   (table) => [uniqueIndex('name_idx').on(table.name)],
